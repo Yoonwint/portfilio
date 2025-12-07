f413b531334d8725de78b52d706495c17c88dd9a
Yoon Wint <skylarburn7@gmail.com>
2025-12-07 12:43:07 +0630

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+const navbar = document.querySelector(".navbar");
+const navbarMenu = document.getElementById("navBar");
+const navLinks = document.querySelectorAll(".nav-link");
+const text = document.querySelector(".multiple-text");
+
+//Preloader
+const preloader = document.querySelector("#preloader");
+if (preloader) {
+  window.addEventListener("load", () => {
+    preloader.remove();
+  });
+}
+
+
+// active nav tab
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    document.querySelector(".active")?.classList.remove("active");
+    navLink.classList.add("active");
+  });
+});
+
+// text animation
+const textArray = ["Web Designer", "Web Developer"];
+let arrayIndex = 1;
+function textLoad(){
+    setInterval(timer, 4000);
+    function timer() {
+        if (arrayIndex < textArray.length){
+            text.innerHTML =  textArray[arrayIndex];
+            arrayIndex = arrayIndex + 1;
+        }else{
+            arrayIndex = 0;
+            text.innerHTML =  textArray[arrayIndex];
+            arrayIndex = arrayIndex + 1;
+        }
+    }
+};
+textLoad();
+
+// scroll on animation
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('show');
+    }else{
+      entry.target.classList.remove('show');
+    }
+  });
+});
+const hiddenObjects = document.querySelectorAll('.hidden');
+hiddenObjects.forEach((e) => observer.observe(e));
+
+x
